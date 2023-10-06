ce723f6016c6125c6eb6ee32efb224823a4fda6f
gabrielfernandesgf <djgabrielfernandes01@gmail.com>
2023-10-05 22:41:50 -0300

    novo case 1

--- src/exAula08/MenuPrincipal.java
+++ src/exAula08/MenuPrincipal.java
@@ -2,15 +2,15 @@ package exAula08;
 
 import java.util.Scanner;
 
-public class MenuPrincipal{
+public class MenuPrincipal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean continuar = true;
 
     while (continuar) {
         System.out.println("Escolha a figura geométrica:");
-        System.out.println("1. Circulo");
-        System.out.println("2. Quadrado");
+        System.out.println("1. Quadrado");
+        System.out.println("2. Circulo");
         System.out.println("3. Triangulo");
         System.out.println("4. Retangulo");
         System.out.println("5. Sair");
@@ -19,8 +19,31 @@ public class MenuPrincipal{
 
         switch (escolha) {
             case 1:
-                Circulo(scanner);
+                System.out.println("Digite a cor do quadrado:");
+                String cor = scanner.next();
+                System.out.println("Digite o lado Do quadrado:");
+                double lado1 = scanner.nextDouble();
+
+                Quadrado quadrado = new Quadrado();
+                quadrado.setCor(cor);
+                quadrado.setLado1(lado1);
+                
+                String result = quadrado.toString();
+                System.out.println(result);
                 break;
+            
+            case 2:
+                System.out.println("aa");
+                break;
+
+            case 3:
+
+            case 4:
+
+            case 5:
+            
+            default:
+                System.out.println("oo");
         }
     }
     }
